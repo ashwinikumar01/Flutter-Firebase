37f1bc5891e749a736ff47360f5aca7e840b6809
Ashwini Kumar Priyanshu <priyanshuashwinikumar@gmail.com>
2020-10-09 11:39:16 +0530

    updated landing page

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -7,29 +7,6 @@ class LandingPage extends StatelessWidget {
   LandingPage({@required this.auth});
   final AuthBase auth;
 
-  // User _user; // variable
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   _checkCurrentUser();
-  //   widget.auth.onAuthStateChanged.listen((user) {
-  //     // stream
-  //     print('user: ${user?.uid}');
-  //   });
-  // }
-
-  // Future<void> _checkCurrentUser() async {
-  //   User user = await widget.auth.currentUser();
-  //   _updateUser(user);
-  // }
-
-  // void _updateUser(User user) {
-  //   setState(() {
-  //     _user = user;
-  //   });
-  // }
-
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<User>(
