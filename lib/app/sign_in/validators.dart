b878cfe27c323036206e4d87e578006d0d55994d
Ashwini Kumar Priyanshu <priyanshuashwinikumar@gmail.com>
2020-10-09 22:08:12 +0530

    validators updated

--- lib/app/sign_in/validators.dart
+++ lib/app/sign_in/validators.dart
@@ -13,5 +13,5 @@ class EmailAndPasswordvalidator {
   final StringValidator emailvalidator = NonEmptyStringValidator();
   final StringValidator passwordvalidator = NonEmptyStringValidator();
   final String invalidEmailErrorText = 'Email can\'t be empty';
-  final String invalidPasswordErrorText = 'Password can\t be empty';
+  final String invalidPasswordErrorText = 'Password can\'t be empty';
 }
