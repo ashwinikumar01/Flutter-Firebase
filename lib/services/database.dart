75b41bdcf230643a8458fd5bce907de225554d31
Ashwini Kumar Priyanshu <priyanshuashwinikumar@gmail.com>
2020-11-03 17:54:59 +0530

    Job entries page

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,20 +1,26 @@
+import 'dart:async';
+
 import 'package:meta/meta.dart';
+import 'package:time_tracker_app/app/home/models/entry.dart';
 import 'package:time_tracker_app/app/home/models/job.dart';
 import 'package:time_tracker_app/services/api_path.dart';
 import 'package:time_tracker_app/services/firestore_service.dart';
 
-// created dynamically
 abstract class Database {
   Future<void> setJob(Job job);
   Future<void> deleteJob(Job job);
   Stream<List<Job>> jobsStream();
+
+  Future<void> setEntry(Entry entry);
+  Future<void> deleteEntry(Entry entry);
+  Stream<List<Entry>> entriesStream({Job job});
 }
 
 String documentIdFromCurrentDate() => DateTime.now().toIso8601String();
 
 class FirestoreDatabase implements Database {
-  final String uid;
   FirestoreDatabase({@required this.uid}) : assert(uid != null);
+  final String uid;
 
   final _service = FirestoreService.instance;
 
@@ -25,29 +31,42 @@ class FirestoreDatabase implements Database {
       );
 
   @override
-  Future<void> deleteJob(Job job) async => _service.deleteData(
-        path: APIPath.job(uid, job.id),
+  Future<void> deleteJob(Job job) async {
+    // delete where entry.jobId == job.jobId
+    final allEntries = await entriesStream(job: job).first;
+    for (Entry entry in allEntries) {
+      if (entry.jobId == job.id) {
+        await deleteEntry(entry);
+      }
+    }
+    // delete job
+    await _service.deleteData(path: APIPath.job(uid, job.id));
+  }
+
+  @override
+  Stream<List<Job>> jobsStream() => _service.collectionStream(
+        path: APIPath.jobs(uid),
+        builder: (data, documentId) => Job.fromMap(data, documentId),
       );
 
-  // converting into list of jobs
-  Stream<List<Job>> jobsStream() => _service.collectionStream(
-      path: APIPath.jobs(uid),
-      builder: (data, documentId) => Job.fromMap(data, documentId));
+  @override
+  Future<void> setEntry(Entry entry) async => await _service.setData(
+        path: APIPath.entry(uid, entry.id),
+        data: entry.toMap(),
+      );
+
+  @override
+  Future<void> deleteEntry(Entry entry) async =>
+      await _service.deleteData(path: APIPath.entry(uid, entry.id));
+
+  @override
+  Stream<List<Entry>> entriesStream({Job job}) =>
+      _service.collectionStream<Entry>(
+        path: APIPath.entries(uid),
+        queryBuilder: job != null
+            ? (query) => query.where('jobId', isEqualTo: job.id)
+            : null,
+        builder: (data, documentID) => Entry.fromMap(data, documentID),
+        sort: (lhs, rhs) => rhs.start.compareTo(lhs.start),
+      );
 }
-
-// created by user
-// abstract class Database {
-//   Future<void> createJob(Map<String, dynamic> jobData);
-// }
-
-// class FirestoreDatabase implements Database {
-//   final String uid;
-//   FirestoreDatabase({@required this.uid}) : assert(uid != null);
-
-// setData returns a Future
-//   Future<void> createJob(Map<String, dynamic> jobData) async {
-//     final path = '/users/$uid/jobs/job_abc';
-//     final documentReference = Firestore.instance.document(path);
-//     await documentReference.setData(jobData);
-//   }
-// }
--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,7 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/services.dart';
 import 'package:meta/meta.dart';
-import 'package:time_tracker_app/common_widgets/platform_exception_alert_dialog.dart';
 
 class FirestoreService {
   FirestoreService._();
@@ -24,12 +22,33 @@ class FirestoreService {
 
   Stream<List<T>> collectionStream<T>({
     @required String path,
-    @required T builder(Map<String, dynamic> data, String documentId),
+    @required T builder(Map<String, dynamic> data, String documentID),
+    Query queryBuilder(Query query),
+    int sort(T lhs, T rhs),
   }) {
-    final reference = Firestore.instance.collection(path);
-    final snapshots = reference.snapshots();
-    return snapshots.map((snapshot) => snapshot.documents
-        .map((snapshot) => builder(snapshot.data, snapshot.documentID))
-        .toList());
+    Query query = Firestore.instance.collection(path);
+    if (queryBuilder != null) {
+      query = queryBuilder(query);
+    }
+    final Stream<QuerySnapshot> snapshots = query.snapshots();
+    return snapshots.map((snapshot) {
+      final result = snapshot.documents
+          .map((snapshot) => builder(snapshot.data, snapshot.documentID))
+          .where((value) => value != null)
+          .toList();
+      if (sort != null) {
+        result.sort(sort);
+      }
+      return result;
+    });
+  }
+
+  Stream<T> documentStream<T>({
+    @required String path,
+    @required T builder(Map<String, dynamic> data, String documentID),
+  }) {
+    final DocumentReference reference = Firestore.instance.document(path);
+    final Stream<DocumentSnapshot> snapshots = reference.snapshots();
+    return snapshots.map((snapshot) => builder(snapshot.data, snapshot.documentID));
   }
 }
